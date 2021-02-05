51b096c5f1f6a9f8673bd674f2963bd704b3c60d
pechuchu <perceive.chuchu@momentum.co.za>
2021-02-05 15:05:49 +0200

    added create student request body to swagger

--- src/main/java/com/college/register/resource/StudentResource.java
+++ src/main/java/com/college/register/resource/StudentResource.java
@@ -8,6 +8,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.modelmapper.ModelMapper;
 
@@ -35,7 +37,8 @@ public class StudentResource {
     
     @POST
     @Produces("application/json")
-    public Student createStudent(@RequestBody StudentDto student) {
+    public Student createStudent(@RequestBody(name = "student", content = @Content(mediaType = "application/json", 
+    	    schema = @Schema(implementation = StudentDto.class)), required = true, description = "create student") StudentDto student) {
         return studentService.createStudent(modelMapper.map(student, Student.class));
     }
 
